092d98861445769d583616c892add2b43fdd53d3
WH <wanghaovip2020@gmail.com>
2016-03-06 21:35:13 +0800

    Update solution.java

--- 19-Remove-Nth-Node-From-End-of-List/solution.java
+++ 19-Remove-Nth-Node-From-End-of-List/solution.java
@@ -17,27 +17,27 @@ public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         
       
-		ListNode headNode = new ListNode(-1);
-		headNode.next = head;
-	    ListNode firstNode = headNode;
-		ListNode secondNode = firstNode;
+	ListNode headNode = new ListNode(-1);
+	headNode.next = head;
+	ListNode firstNode = headNode;
+	ListNode secondNode = firstNode;
 
-		// 移动第二个节点到第一个节点的n个后面的位置
-		int i = 0;
-		while (i < n) {
-			secondNode = secondNode.next;
-			i++;
-		}
+	// 移动第二个节点到第一个节点的n个后面的位置
+	int i = 0;
+	while (i < n) {
+		secondNode = secondNode.next;
+		i++;
+	}
 
-		while ( secondNode.next != null) {
-			firstNode = firstNode.next;
-			secondNode = secondNode.next;
-		}
+	while ( secondNode.next != null) {
+		firstNode = firstNode.next;
+		secondNode = secondNode.next;
+	}
 
-		// 删除第N个节点
+	// 删除第N个节点
         firstNode.next = firstNode.next.next;
 
-		return headNode.next;
+	return headNode.next;
         
     }
 }
